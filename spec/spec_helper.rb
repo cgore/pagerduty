d323d89a3fde2cf264a03efe023cdb858221f6d2
Christopher Mark Gore <cgore@cgore.com>
2014-08-04 10:06:24 -0500

    Enabling :should syntax for RSpec.
    RSpec 3 needs :should to be enabled by default.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,10 @@ Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.color = true
+  config.expect_with :rspec do |c|
+    c.syntax = [:should,:expect]
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = [:should,:expect]
+  end
 end
